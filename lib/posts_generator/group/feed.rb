e1036b52d32d74622cb72672477158753d843fcf
Mike Rogers <me@mikerogers.io>
2021-01-04 23:17:48 +0000

    Adding RUG::B

--- lib/posts_generator/group/feed.rb
+++ lib/posts_generator/group/feed.rb
@@ -1,7 +1,25 @@
+require 'net/http'
+require 'uri'
+require 'json'
+
 class PostsGenerator::Group::Feed < PostsGenerator::Group
   private
 
   def events
-    []
+    @events ||= source_data.collect do |event_data|
+      Event.new(
+        title: event_data['name'],
+        datetime: Time.parse(event_data['date']),
+        url: "#{@group['url']}/events/#{event_data['name'].parameterize}-#{event_data['id']}",
+        name: name
+      )
+    end
+  end
+
+  def source_data
+    @source_data ||= begin
+                       json = Net::HTTP.get(URI.parse("#{@group['feed_url']}"))
+                       JSON.parse(json)
+                     end
   end
 end
