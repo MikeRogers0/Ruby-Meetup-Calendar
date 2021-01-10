216b5ffcaeb2755357ff6a53cfbb2327a8197af5
Mike Rogers <me@mikerogers.io>
2021-01-10 20:03:33 +0000

    Reducing amount of tailwind outputted

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   future: {
     defaultLineHeights: true,
@@ -5,19 +7,47 @@ module.exports = {
     removeDeprecatedGapUtilities: true,
     standardFontWeights: true
   },
-  purge: {
-    content: [
-      "./output/**/*.html",
-      "./frontend/**/*.js",
-      "./src/**/*.md",
-      "./src/**/*.html",
-      "./src/**/*.liquid"
-    ]
-  },
   theme: {
+    spacing: {
+      px: '1px',
+      0: '0',
+      0.5: '0.125rem',
+      1: '0.25rem',
+      1.5: '0.375rem',
+      2: '0.5rem',
+      2.5: '0.625rem',
+      3: '0.75rem',
+      3.5: '0.875rem',
+      4: '1rem',
+      5: '1.25rem',
+      6: '1.5rem',
+      7: '1.75rem',
+      8: '2rem',
+      9: '2.25rem',
+      10: '2.5rem',
+      11: '2.75rem',
+      12: '3rem',
+      14: '3.5rem',
+      16: '4rem',
+      20: '5rem',
+      24: '6rem',
+      28: '7rem',
+      32: '8rem'
+    },
+    screens: {
+      md: '768px',
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      blue: colors.blue,
+    },
     extend: {
-       minHeight: {
-         '28': '7rem' 
+      minHeight: {
+        '28': '7rem'
       }
     }
   },
@@ -26,6 +56,5 @@ module.exports = {
     // add this line if you use tailwindui
     // require('@tailwindcss/forms'),
     require("@tailwindcss/typography"),
-    require("@tailwindcss/aspect-ratio")
   ]
 };
