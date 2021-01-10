d3516e58940b1f9340718caa5ea9d0cb5277bb53
Mike Rogers <me@mikerogers.io>
2021-01-10 21:16:29 +0000

    standardrb --fix

--- lib/posts_generator/group/meetup.rb
+++ lib/posts_generator/group/meetup.rb
@@ -1,29 +1,29 @@
-require 'net/http'
-require 'uri'
-require 'json'
+require "net/http"
+require "uri"
+require "json"
 
 class PostsGenerator::Group::Meetup < PostsGenerator::Group
   private
 
   def events
-    @events ||= source_data.collect do |event_data|
+    @events ||= source_data.collect { |event_data|
       Event.new(
-        title: event_data['title'],
-        datetime: Time.parse(event_data['servertime']),
-        url: event_data['event_url'],
+        title: event_data["title"],
+        datetime: Time.parse(event_data["servertime"]),
+        url: event_data["event_url"],
         name: name
       )
-    end
+    }
   end
 
   def meetup_id
-    @group['meetup_id']
+    @group["meetup_id"]
   end
 
   def source_data
     @source_data ||= begin
-                       json = Net::HTTP.get(URI.parse("https://www.meetup.com/#{meetup_id}/events/json/"))
-                       JSON.parse(json)
-                     end
+      json = Net::HTTP.get(URI.parse("https://www.meetup.com/#{meetup_id}/events/json/"))
+      JSON.parse(json)
+    end
   end
 end
--- lib/posts_generator/group/on_ruby_feed.rb
+++ lib/posts_generator/group/on_ruby_feed.rb
@@ -1,25 +1,25 @@
-require 'net/http'
-require 'uri'
-require 'json'
+require "net/http"
+require "uri"
+require "json"
 
 class PostsGenerator::Group::OnRubyFeed < PostsGenerator::Group
   private
 
   def events
-    @events ||= source_data.collect do |event_data|
+    @events ||= source_data.collect { |event_data|
       Event.new(
-        title: event_data['name'],
-        datetime: Time.parse(event_data['date']),
-        url: "#{@group['url']}events/#{event_data['name'].parameterize}-#{event_data['id']}",
+        title: event_data["name"],
+        datetime: Time.parse(event_data["date"]),
+        url: "#{@group["url"]}events/#{event_data["name"].parameterize}-#{event_data["id"]}",
         name: name
       )
-    end
+    }
   end
 
   def source_data
     @source_data ||= begin
-                       json = Net::HTTP.get(URI.parse("#{@group['feed_url']}"))
-                       JSON.parse(json)
-                     end
+      json = Net::HTTP.get(URI.parse((@group["feed_url"]).to_s))
+      JSON.parse(json)
+    end
   end
 end
