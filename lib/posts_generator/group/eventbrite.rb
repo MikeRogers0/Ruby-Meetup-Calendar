bdfd76b7baa8dc804e391b0ce8ff3ef8a05d83d0
Mike Rogers <me@mikerogers.io>
2021-01-17 15:07:58 +0000

    Pull events data from EventBrite (#8)
    * First go at using the Eventbrite API
    
    * Running the rake task to pull in eventbrite events

--- lib/posts_generator/group/eventbrite.rb
+++ lib/posts_generator/group/eventbrite.rb
@@ -2,6 +2,25 @@ class PostsGenerator::Group::Eventbrite < PostsGenerator::Group
   private
 
   def events
-    []
+    @events ||= source_data.collect { |event_data|
+      Event.new(
+        title: event_data["name"],
+        datetime: Time.parse(event_data["start_date"] + " " + event_data["start_time"] + " " + event_data["timezone"]),
+        url: event_data["url"],
+        name: name
+      )
+    }
+  end
+
+  # This is the primary_organizer_id field on Meetup
+  def eventbrite_id
+    @group["eventbrite_id"]
+  end
+
+  def source_data
+    @source_data ||= begin
+      json = Net::HTTP.get(URI.parse("https://www.eventbrite.com/api/v3/destination/organizers/#{eventbrite_id}/events/?order_by=created_desc&time_filter=current_future"))
+      JSON.parse(json)["events"]
+    end
   end
 end
---
+++ src/_events/2021-04-15-north-west-ruby-user-group.md
@@ -0,0 +1,8 @@
+---
+layout: post
+title: North West Ruby User Group - 2021-04-15
+datetime: 2021-04-15 13:30:00.000000000 -04:00
+name: North West Ruby User Group
+external_url: https://www.meetup.com/North-West-Ruby-User-Group/events/jdlpqqyccgbtb/
+year_month: 2021-04
+---
