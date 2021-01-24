a83fe6b18996c35288ef3caefde8beafccd4a061
Mike Rogers <me@mikerogers.io>
2021-01-24 17:56:40 +0000

    standardrb --fix

--- lib/events_reformatter.rb
+++ lib/events_reformatter.rb
@@ -1,7 +1,7 @@
 require "yaml"
 require "active_support"
 require "active_support/core_ext"
-require "./lib/event.rb"
+require "./lib/event"
 
 class EventsReformatter
   EVENTS_PATH = "src/_events/*.md"
@@ -10,14 +10,13 @@ class EventsReformatter
     events.each do |event_path|
       event_data = YAML.safe_load(File.open(event_path))
       Event.new(
-        title: event_data['title'],
-        datetime: Time.parse(event_data['datetime']),
-        url: event_data['external_url'],
-        name: event_data['name'], 
-        online_event: event_data['online_event']
+        title: event_data["title"],
+        datetime: Time.parse(event_data["datetime"]),
+        url: event_data["external_url"],
+        name: event_data["name"],
+        online_event: event_data["online_event"]
       ).save
     end
-  
   end
 
   private
