ad67c8f6b671b4bc94f707ab8efb69b671ac5fa8
Meg Gutshall <meghan.gutshall@gmail.com>
2021-03-26 07:34:26 -0400

    Tailwind Styling (#35)
    * Remove unneeded default values
    
    * Extend minHeight syntax
    
    Check the docs on this one: https://tailwindcss.com/docs/min-height#customizing
    
    * Fix Tailwind config syntax
    
    It looks like you were trying to disable the `clear` and `float` properties. This is how you do that:
    
    - https://tailwindcss.com/docs/clear#customizing
    - https://tailwindcss.com/docs/float#customizing

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,35 +7,11 @@ module.exports = {
     removeDeprecatedGapUtilities: true,
     standardFontWeights: true
   },
+  corePlugins: {
+    clear: false,
+    float: false
+  },
   theme: {
-    clear: [],
-    float: [],
-    spacing: {
-      px: '1px',
-      0: '0',
-      0.5: '0.125rem',
-      1: '0.25rem',
-      1.5: '0.375rem',
-      2: '0.5rem',
-      2.5: '0.625rem',
-      3: '0.75rem',
-      3.5: '0.875rem',
-      4: '1rem',
-      5: '1.25rem',
-      6: '1.5rem',
-      7: '1.75rem',
-      8: '2rem',
-      9: '2.25rem',
-      10: '2.5rem',
-      11: '2.75rem',
-      12: '3rem',
-      14: '3.5rem',
-      16: '4rem',
-      20: '5rem',
-      24: '6rem',
-      28: '7rem',
-      32: '8rem'
-    },
     colors: {
       transparent: 'transparent',
       current: 'currentColor',
@@ -47,6 +23,7 @@ module.exports = {
     extend: {
       minHeight: {
         '28': '7rem'
+        // What are you trying to do here? This doesn't make sense with the documentation: https://tailwindcss.com/docs/min-height#customizing.
       }
     }
   },
