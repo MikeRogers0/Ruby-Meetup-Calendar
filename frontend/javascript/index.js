11359cef02f6b2ca033e4ebdd51b1fafcd293cd0
Mike Rogers <me@mikerogers.io>
2021-04-23 19:55:04 +0100

    Linking to SVGs over storing them in JS file, now turbo works

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -5,9 +5,6 @@ import * as Turbo from "@hotwired/turbo"
 import LocalTime from "local-time"
 LocalTime.start()
 
-import feather from "feather-icons"
-feather.replace();
-
 // Import all javascript files from src/_components
 const componentsContext = require.context("bridgetownComponents", true, /.js$/)
 componentsContext.keys().forEach(componentsContext)
