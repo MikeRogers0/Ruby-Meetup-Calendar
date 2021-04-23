8a5a4ad7164f0e8629a8d699f12e844668052c40
oreoluwa <oreoluwa@outlook.com>
2021-04-22 22:55:49 -0400

    Support meetingplace.io(https://meetingplace.io/api). Add indy.rb

---
+++ lib/posts_generator/group/meeting_place.rb
@@ -0,0 +1,30 @@
+require "net/http"
+require "uri"
+require "json"
+
+class PostsGenerator::Group::MeetingPlace < PostsGenerator::Group
+  private
+
+  def events
+    @events ||= source_data.collect { |event_data|
+      Event.new(
+        title: event_data["name"],
+        datetime: Time.parse(event_data["start_time"]),
+        url: event_data["url"],
+        online_event: event_data["location"] == 'Online Event',
+        name: name
+      )
+    }
+  end
+
+  def meeting_place_id
+    @group["meeting_place_id"]
+  end
+
+  def source_data
+    @source_data ||= begin
+      json = Net::HTTP.get(URI.parse("https://meetingplace.io/api/v1/group/#{meeting_place_id}/events"))
+      JSON.parse(json)
+    end
+  end
+end
