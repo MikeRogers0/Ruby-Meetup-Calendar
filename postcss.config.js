7bf116ef02086bda574d43d30949ff97aed2db47
Mike Rogers <me@mikerogers.io>
2021-05-05 17:21:34 +0100

    Using PostCSS for media queries

--- postcss.config.js
+++ postcss.config.js
@@ -3,10 +3,16 @@ module.exports = {
     'postcss-import-ext-glob': {},
     'postcss-flexbugs-fixes': {},
     'postcss-preset-env': {
+      importFrom: [
+        './frontend/styles/config/breakpoints.json',
+        './frontend/styles/config/colours.css',
+        './frontend/styles/config/fonts.css',
+        './frontend/styles/config/spacing.css',
+      ],
       autoprefixer: {
         flexbox: 'no-2009'
       },
-      stage: 3
+      stage: 1
     }
   }
 }
