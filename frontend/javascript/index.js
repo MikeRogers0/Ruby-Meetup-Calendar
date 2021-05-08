77f424dbb807fa4b437b6796f69322e7d36e428b
Mike Rogers <me@mikerogers.io>
2021-05-08 13:29:39 +0100

    Adding Turbolinks (Speed up navigation rendering time)

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -6,3 +6,6 @@ LocalTime.start()
 // Import the React Components
 const components = require.context('./components', true, /.js$/)
 components.keys().forEach(components)
+
+var Turbolinks = require("turbolinks")
+Turbolinks.start()
